df0fbd5313826fcb2d6e665674e480d353e9ceb8
fran0770 <franmowat33@gmail.com>
2024-01-10 17:46:53 +0000

    styling validation messages

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-let form = document.querySelector("form");
-console.log(form.checkValidity());
+//let form = document.querySelector("form");
+//console.log(form.checkValidity());
 
 let checkInputValidity = (e) => {
-    if (e.target.value.length == 1 || e.target.value.length >= 30){
-        e.target.nextElementSibling.textContent = "Value must be between 2 and 30 characters.";
-    } else if (e.target.value.length == 0){
-        e.target.nextElementSibling.textContent = "* This field is required";
-    } else if (! e.target.checkValidity()){
-        e.target.nextElementSibling.textContent = "Invalid value";
-    } else{
-        e.target.nextElementSibling.textContent = "";
+    let input = e.target; 
+    let errorMessage = input.nextElementSibling;
+
+    if (input.value.length == 1){
+        errorMessage.textContent = input.id[0].toUpperCase() + input.id.slice(1) + " requires minimum 2 characters.";
+        input.style.border = "1px solid #ff3f3f";
+    } else if (input.value.length >= 30){
+        errorMessage.textContent = "Value must be less than 30 characters.";
+        input.style.border = "1px solid #ff3f3f";
+    } else if (input.value.length == 0){
+        errorMessage.textContent = "* This field is required.";
+        input.style.border = "1px solid #ff3f3f";
+    } else if (! input.checkValidity()){
+        errorMessage.textContent = "Please enter a valid " + input.id + ".";
+        input.style.border = "1px solid #ff3f3f";
+    } else {
+        errorMessage.textContent = "";
+        input.style.border = "1px solid #E5E7EB";
     }
 }
 
-let firstName = document.getElementById("first-name");
+let firstName = document.getElementById("name");
 firstName.addEventListener("keyup", checkInputValidity);
 
 let surname = document.getElementById("surname");
